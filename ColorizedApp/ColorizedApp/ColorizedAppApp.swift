5a9dcabf72115beb0025d245a0965141a19bc3cc
Orianka13 <odobrogor@gmail.com>
2023-05-13 11:22:17 +0400

    Создан проект

---
+++ ColorizedApp/ColorizedApp/ColorizedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppApp.swift
+//  ColorizedApp
+//
+//  Created by Олеся Егорова on 13.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
